02ed7d350c33f01c9b5b1474a24f50e1c77ba73d
Kamil <millos10@wp.pl>
2023-04-21 00:07:11 +0200

    test puling from key vault

--- testKeyVault/Pages/Index.cshtml.cs
+++ testKeyVault/Pages/Index.cshtml.cs
@@ -6,15 +6,19 @@ namespace testKeyVault.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _conf;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration conf)
         {
             _logger = logger;
+            _conf = conf;
         }
+
+        public string Key { get; set; }
 
         public void OnGet()
         {
-
+            Key = _conf["UserSecret"];
         }
     }
 }
